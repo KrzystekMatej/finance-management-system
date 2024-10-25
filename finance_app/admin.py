31b4a30dd0c6a11c2815c26aa49148b686285b20
KrzystekMatej <78759642+KrzystekMatej@users.noreply.github.com>
2024-10-26 00:38:35 +0200

    Models update and tests

--- finance_app/admin.py
+++ finance_app/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from finance_app.models import *
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(CategoryPreference)
+admin.site.register(Transaction)
+admin.site.register(RecurringTransaction)
+admin.site.register(Budget)
+admin.site.register(SharedBudget)
+admin.site.register(Notification)
